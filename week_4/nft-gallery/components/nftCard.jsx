e1453d302f393856a4cff19f4dc18bb0d29f2fea
Your Namedukep <dukepato2010@outlook.com>
2022-06-07 22:56:07 +0100

    final commit

--- week_4/nft-gallery/components/nftCard.jsx
+++ week_4/nft-gallery/components/nftCard.jsx
@@ -3,19 +3,21 @@ export const NFTCard = ({ nft }) => {
     return (
         <div className="w-1/4 flex flex-col ">
         <div className="rounded-md">
-            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        <a target={"_blank"} href={`https://mumbai.polygonscan.com/address/${nft.contract.address}`}> 
+        <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </a>
         </div>
         <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
             <div className="">
                 <h2 className="text-xl text-gray-800">{nft.title}</h2>
                 <p className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-                <p className="text-gray-600" >{`${nft.contract.address.substr(0, 5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+                <p className="text-gray-600" >Contract: {`${nft.contract.address.substr(0, 5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
             </div>
             <div className="flex-grow mt-2">
-                <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
+                <p className="text-gray-600">Description: {nft.description?.substr(0, 150)}</p>
             </div>
             <div>
-                <a href={`https://mumbai.polygonscan.com/address/${nft.contract.address}`}> View on PolygonSacn</a>
+                <a target={"_blank"} href={`https://mumbai.polygonscan.com/address/${nft.contract.address}`}> View on PolygonSacn</a>
             </div>
         </div>
 
